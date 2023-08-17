e9050bdfb11b41f4d85923c6b6115a4822a93e07
Manzareka <mi.machitidze@gmail.com>
2023-08-17 16:44:48 +0400

    Add header & introduction sections
    Add header & introduction sections

---
+++ src/js/scrypt.js
@@ -0,0 +1,39 @@
+"use strict";
+
+// HEADER SLIDER
+
+function initSlider() {
+  const slides = document.querySelectorAll(".slide");
+  const sliderWrapper = document.querySelector(".slider-wrapper");
+
+  let activeIndex = 0;
+
+  function renderSlides() {
+    slides.forEach((slide, index) => {
+      if (activeIndex === index) {
+        slide.classList.add("active");
+      } else {
+        slide.classList.remove("active");
+      }
+    });
+  }
+  renderSlides();
+  function showNext() {
+    if (activeIndex === slides.length - 1) {
+      activeIndex = 0;
+    } else {
+      activeIndex++;
+    }
+    renderSlides();
+  }
+
+  let sliderIntervalId = null;
+
+  function startAutoSliderFn() {
+    sliderIntervalId = setInterval(showNext, 3000);
+  }
+
+  startAutoSliderFn();
+}
+
+initSlider();
